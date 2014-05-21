d20df6a28669b08e73e6eb4e7f04cbb454d7a21a
lassic <homedev@gmail.com>
2014-05-21 15:55:29 +0300

    example using FB ProfilePictureView and refactor some stuff

--- SoomlaSocialExample/src/main/java/com/soomla/social/example/FacebookExampleFragment.java
+++ SoomlaSocialExample/src/main/java/com/soomla/social/example/FacebookExampleFragment.java
@@ -20,13 +20,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.facebook.Request;
-import com.facebook.Session;
-import com.facebook.android.Facebook;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.FacebookDialog;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
+import com.soomla.social.example.util.ImageUtils;
 
 import java.util.Arrays;
 
@@ -35,7 +39,13 @@ import java.util.Arrays;
  */
 public class FacebookExampleFragment extends FacebookEnabledFragment {
 
-    private Button btnShareDlg;
+    private Button mBtnShareDlg;
+
+    private ViewGroup mProfileBar;
+    private ImageView mProfileAvatar;
+    private TextView mProfileName;
+
+    private ProfilePictureView mProfilePicView;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -45,14 +55,43 @@ public class FacebookExampleFragment extends FacebookEnabledFragment {
 
         View view = inflater.inflate(R.layout.fb_example_main, container, false);
 
-        LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+        mProfileBar = (ViewGroup) view.findViewById(R.id.profile_bar);
+        mProfileAvatar = (ImageView) view.findViewById(R.id.prof_avatar);
+        mProfileName = (TextView) view.findViewById(R.id.prof_name);
 
+        mProfilePicView = (ProfilePictureView) view.findViewById(R.id.fb_profile_picview);
+
+        LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
         authButton.setFragment(this);
         authButton.setReadPermissions(Arrays.asList("public_profile"));
         //authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
 
-        btnShareDlg = (Button) view.findViewById(R.id.btnShareDlg);
-        btnShareDlg.setOnClickListener(new View.OnClickListener() {
+        authButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                showView(mProfileBar, true);
+
+                final String userFullName = user.getFirstName() + " " + user.getLastName();
+                mProfileName.setText(userFullName);
+
+                // 1. custom UI+fetch
+                if(mProfileAvatar != null) {
+                    String imageUrl = "http://graph.facebook.com/" + user.getId() + "/picture";
+                    new ImageUtils.DownloadImageTask(mProfileAvatar).execute(imageUrl);
+                }
+                else { // 2. FB UI
+                    if(mProfilePicView != null) {
+                        mProfilePicView.setProfileId(user.getId());
+                    }
+                }
+
+//                new ImageUtils.DownloadImageTask(mProfileAvatar).execute(profileEvent.profile.getProfileImageURL());
+//                mProfileName.setText(profileEvent.profile.getFullName());
+            }
+        });
+
+        mBtnShareDlg = (Button) view.findViewById(R.id.btnShareDlg);
+        mBtnShareDlg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (FacebookDialog.canPresentShareDialog(getActivity().getApplicationContext(),
@@ -60,7 +99,7 @@ public class FacebookExampleFragment extends FacebookEnabledFragment {
                     // Publish the post using the Share Dialog
                     FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(getActivity())
                             .setFragment(FacebookExampleFragment.this)
-                            // null link is status update
+                                    // null link is status update
                             .setLink("https://developers.facebook.com/android")
                             .build();
                     uiHelper.trackPendingDialogCall(shareDialog.present());
@@ -75,15 +114,24 @@ public class FacebookExampleFragment extends FacebookEnabledFragment {
         return view;
     }
 
+    private void showView(final View view, boolean show) {
+        final Animation animation = show ?
+                AnimationUtils.makeInAnimation(view.getContext(), true) :
+                AnimationUtils.makeOutAnimation(view.getContext(), true);
+        animation.setFillAfter(true);
+        animation.setDuration(500);
+        view.startAnimation(animation);
+    }
+
     @Override
     protected void onLogin() {
         super.onLogin();
-        btnShareDlg.setVisibility(View.VISIBLE);
+        mBtnShareDlg.setVisibility(View.VISIBLE);
     }
 
     @Override
     protected void onLogout() {
         super.onLogout();
-        btnShareDlg.setVisibility(View.GONE);
+        mBtnShareDlg.setVisibility(View.GONE);
     }
 }
--- SoomlaSocialExample/src/main/java/com/soomla/social/example/SocialAuthExampleActivity.java
+++ SoomlaSocialExample/src/main/java/com/soomla/social/example/SocialAuthExampleActivity.java
@@ -44,6 +44,7 @@ import com.soomla.social.actions.UpdateStoryAction;
 import com.soomla.social.events.SocialActionPerformedEvent;
 import com.soomla.social.events.SocialAuthProfileEvent;
 import com.soomla.social.events.SocialLoginEvent;
+import com.soomla.social.example.util.ImageUtils;
 import com.soomla.social.model.SocialVirtualItemReward;
 import com.soomla.store.BusProvider;
 import com.squareup.otto.Subscribe;
@@ -171,7 +172,7 @@ public class SocialAuthExampleActivity extends ActionBarActivity {
     @Subscribe public void onSocialProfileEvent(SocialAuthProfileEvent profileEvent) {
         showView(mProfileBar, true);
 
-        new DownloadImageTask(mProfileAvatar).execute(profileEvent.profile.getProfileImageURL());
+        new ImageUtils.DownloadImageTask(mProfileAvatar).execute(profileEvent.profile.getProfileImageURL());
         mProfileName.setText(profileEvent.profile.getFullName());
     }
 
@@ -262,120 +263,5 @@ public class SocialAuthExampleActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String url = urls[0];
-            Bitmap bmp = downloadBitmapWithClient(url);
-
-            return bmp;
-        }
-
-        // doesn't follow https redirect!
-        private Bitmap downloadBitmap(String stringUrl) {
-            URL url = null;
-            HttpURLConnection connection = null;
-            InputStream inputStream = null;
-
-            try {
-                url = new URL(stringUrl);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setUseCaches(true);
-                inputStream = connection.getInputStream();
-
-                return BitmapFactory.decodeStream(new FlushedInputStream(inputStream));
-            } catch (Exception e) {
-                Log.w(TAG, "Error while retrieving bitmap from " + stringUrl, e);
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-
-            return null;
-        }
-
-        private Bitmap downloadBitmapWithClient(String url) {
-            final AndroidHttpClient httpClient = AndroidHttpClient.newInstance("Android");
-            HttpClientParams.setRedirecting(httpClient.getParams(), true);
-            final HttpGet request = new HttpGet(url);
-
-            try {
-                HttpResponse response = httpClient.execute(request);
-                final int statusCode = response.getStatusLine().getStatusCode();
-
-                if (statusCode != HttpStatus.SC_OK) {
-                    Header[] headers = response.getHeaders("Location");
-
-                    if (headers != null && headers.length != 0) {
-                        String newUrl = headers[headers.length - 1].getValue();
-                        // call again with new URL
-                        return downloadBitmap(newUrl);
-                    } else {
-                        return null;
-                    }
-                }
-
-                final HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    InputStream inputStream = null;
-                    try {
-                        inputStream = entity.getContent();
-
-                        // do your work here
-                        return BitmapFactory.decodeStream(inputStream);
-                    } finally {
-                        if (inputStream != null) {
-                            inputStream.close();
-                        }
-                        entity.consumeContent();
-                    }
-                }
-            } catch (Exception e) {
-                request.abort();
-            } finally {
-                if (httpClient != null) {
-                    httpClient.close();
-                }
-            }
-
-            return null;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
-
-    static class FlushedInputStream extends FilterInputStream {
-        public FlushedInputStream(InputStream inputStream) {
-            super(inputStream);
-        }
-
-        @Override
-        public long skip(long n) throws IOException {
-            long totalBytesSkipped = 0L;
-            while (totalBytesSkipped < n) {
-                long bytesSkipped = in.skip(n - totalBytesSkipped);
-                if (bytesSkipped == 0L) {
-                    int byteValue = read();
-                    if (byteValue < 0) {
-                        break; // we reached EOF
-                    } else
-                    {
-                        bytesSkipped = 1; // we read one byte
-                    }
-                }
-                totalBytesSkipped += bytesSkipped;
-            }
-            return totalBytesSkipped;
-        }
     }
 }
