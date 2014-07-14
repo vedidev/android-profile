eede47046a45653667066a8d3492cfadc3adb573
Lassic <homedev@gmail.com>
2014-07-14 18:01:31 +0300

    fix event field

--- SoomlaAndroidProfile/src/com/soomla/profile/events/social/GetFeedFailedEvent.java
+++ SoomlaAndroidProfile/src/com/soomla/profile/events/social/GetFeedFailedEvent.java
@@ -6,9 +6,9 @@ import com.soomla.profile.social.ISocialProvider;
  * Created by oriargov on 7/14/14.
  */
 public class GetFeedFailedEvent extends BaseSocialActionEvent {
-    public final String Message;
-    public GetFeedFailedEvent(ISocialProvider.SocialActionType getFeedType, String message) {
+    public final String ErrorDescription;
+    public GetFeedFailedEvent(ISocialProvider.SocialActionType getFeedType, String errorDescription) {
         super(getFeedType);
-        Message = message;
+        ErrorDescription = errorDescription;
     }
 }
