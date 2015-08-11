5f4fe2a61999a3c061a8dc4772e5512e46e593c2
Boris Spektor <boris@soom.la>
2015-08-11 14:54:24 +0300

    Added version and rebuilt jars

--- SoomlaAndroidProfile/src/com/soomla/profile/SoomlaProfile.java
+++ SoomlaAndroidProfile/src/com/soomla/profile/SoomlaProfile.java
@@ -45,6 +45,8 @@ import java.util.Map;
 @SuppressWarnings("UnusedDeclaration")
 public class SoomlaProfile {
 
+    public static final String VERSION = "1.1.3";
+
     /**
      * see {@link #initialize(Activity, boolean, Map)}
      */
